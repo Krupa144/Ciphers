5a4d2501fbde6bdd73aa8bac25e39f6f951bd7ff
Krupa144 <kacpi8450@gmail.com>
2024-12-02 17:48:51 +0100

    adding polibus chisper tomy project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,7 +48,7 @@ namespace Chpiers.Controllers
 
         private string EncryptWithCaesarCipher(string text, int shift)
         {
-            shift = shift % 26; 
+            shift = shift % 26;
             return new string(text.Select(c =>
             {
                 if (char.IsLetter(c))
@@ -60,6 +60,45 @@ namespace Chpiers.Controllers
             }).ToArray());
         }
 
+        public IActionResult PolybiusCipher() => View();
+
+        [HttpPost]
+        public IActionResult PolybiusCipher(string inputText)
+        {
+            if (string.IsNullOrWhiteSpace(inputText))
+            {
+                ViewBag.Result = "Wprowadü tekst do zaszyfrowania!";
+            }
+            else
+            {
+                ViewBag.Result = EncryptWithPolybiusCipher(inputText);
+            }
+
+            return View();
+        }
+
+        private string EncryptWithPolybiusCipher(string text)
+        {
+            // Tabela szyfru Polibiusza
+            var polybiusSquare = new Dictionary<char, string>
+    {
+        {'A', "11"}, {'B', "12"}, {'C', "13"}, {'D', "14"}, {'E', "15"},
+        {'F', "21"}, {'G', "22"}, {'H', "23"}, {'I', "24"}, {'J', "24"}, // I i J majπ ten sam kod
+        {'K', "25"}, {'L', "31"}, {'M', "32"}, {'N', "33"}, {'O', "34"},
+        {'P', "35"}, {'Q', "41"}, {'R', "42"}, {'S', "43"}, {'T', "44"},
+        {'U', "45"}, {'V', "51"}, {'W', "52"}, {'X', "53"}, {'Y', "54"},
+        {'Z', "55"}
+    };
+
+            // Zamiana tekstu na wielkie litery i szyfrowanie
+            text = text.ToUpper();
+            var encrypted = text
+                .Where(c => char.IsLetter(c)) // Ignorowanie znakÛw nie bÍdπcych literami
+                .Select(c => polybiusSquare.ContainsKey(c) ? polybiusSquare[c] : "")
+                .ToArray();
+
+            return string.Join(" ", encrypted);
+        }
 
     }
 }
